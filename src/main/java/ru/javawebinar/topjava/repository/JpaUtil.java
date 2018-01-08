b5d19957f46f3640e48e6273103ffbf692578040
PeucT <Cool_math@mail.ru>
2018-01-08 21:35:50 +0300

    HW06_initial_empty_commit

--- src/main/java/ru/javawebinar/topjava/repository/JpaUtil.java
+++ src/main/java/ru/javawebinar/topjava/repository/JpaUtil.java
@@ -11,6 +11,7 @@ public class JpaUtil {
     @PersistenceContext
     private EntityManager em;
 
+    
     public void clear2ndLevelHibernateCache() {
         Session s = (Session) em.getDelegate();
         SessionFactory sf = s.getSessionFactory();
